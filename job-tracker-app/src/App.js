2144f71ec435aaaf71f31c047bad12307be703d4
Sumit Das <dassumit.ece@gmail.com>
2023-04-01 11:42:27 -0600

    Auth0 integration completed

--- job-tracker-app/src/App.js
+++ job-tracker-app/src/App.js
@@ -1,10 +1,20 @@
+import {Auth0Provider} from '@auth0/auth0-react'
+import LoginButton from './components/LoginButton';
+import Profile from './components/Profile'
+const domain = process.env.REACT_APP_JOB_TRACKER_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_JOB_TRACKER_APP_AUTH0_CLIENT_ID;
 
-
-function App() {
+const App = () => {
   return (
-    <div>
-      <h1>Test</h1>
-    </div>
+    <Auth0Provider responseType = 'code' domain={domain} clientId= {clientId}    authorizationParams={{
+      redirect_uri: window.location.origin,
+      audience: "https://dev-27tgvrxpujzp1vcw.us.auth0.com/api/v2/",
+      scope: "openid profile email"
+    }}>
+      <LoginButton />
+      <Profile />
+
+    </Auth0Provider>
   );
 }
 
