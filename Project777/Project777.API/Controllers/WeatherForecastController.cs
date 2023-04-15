cc2743cc31e3f632bdca7231ebc13632f48a698f
Jean Armstrong <bjarmstr@yahoo.com>
2023-04-15 13:03:41 -0600

    JMA add an endpoint with Authorization

--- Project777/Project777.API/Controllers/WeatherForecastController.cs
+++ Project777/Project777.API/Controllers/WeatherForecastController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Project777.API.Controllers
@@ -20,6 +21,19 @@ namespace Project777.API.Controllers
 
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("auth")]
+        [Authorize]
+        public IEnumerable<WeatherForecast> Get2()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
