9721ee6976bf39c194b989007811aa7c1d4c9261
jmeng-sys <charlesmeng268@gmail.com>
2023-07-06 21:13:00 -0600

    built a job controller, added Migration, ongoing

---
+++ Project777/Project777.Service/Interfaces/IJobService.cs
@@ -0,0 +1,14 @@
+﻿using Project777.Models.ViewModels.Jobs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project777.Services.Interfaces
+{
+    public interface IJobService
+    {
+        public Task<JobVM> Create(CreateJobVM jobAdd, string userId);
+    }
+}
